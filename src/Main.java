e8ab74841ef34e05c62e4d5069294c3b1539c479
markup54 <marzenakupka@tlen.pl>
2024-09-06 11:38:34 +0200

    zadanie z haslem

--- src/Main.java
+++ src/Main.java
@@ -88,5 +88,26 @@ public class Main {
         }
         while (wpisanaLiczba != wylosowanaLiczba);
         System.out.println("Wygrana");
+        //Pierwsze zadanie domowe zadanie8
+        //Wygeneruj hasło losowe. Długość hasła powinna wynosić 20 znaków, a ponadto powinno zawierać liczby, wielkie i małe litery oraz znaki specjalne.
+        String haslo ="";
+        String maleLitery = "qwertyuioplkjhgfdsazxcvbnm";
+        String duzeLitery = "QWERTYUIOPLKJHGFDSAZXCVBNM";
+        String znakiSpecjalne = "!@#$%^&*()_+=-><,.?/|";
+        String cyfry = "0123456789";
+        int losowa;
+
+        for (int i = 0; i < 5; i++) {
+            losowa= (int) (Math.random()*maleLitery.length());
+            haslo = haslo + maleLitery.charAt(losowa);
+            losowa= (int) (Math.random()*duzeLitery.length());
+            haslo = haslo + duzeLitery.charAt(losowa);
+            losowa= (int) (Math.random()*znakiSpecjalne.length());
+            haslo = haslo + znakiSpecjalne.charAt(losowa);
+            losowa= (int) (Math.random()*cyfry.length());
+            haslo = haslo + cyfry.charAt(losowa);
+        }
+        System.out.println("Wygenerowane hasło: "+haslo);
+
     }
 }
