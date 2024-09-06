5910464487b2446153e86e693b423e090e26fd95
markup54 <marzenakupka@tlen.pl>
2024-09-06 10:26:05 +0200

    wczytywanie liczby z klawiatury

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
 
@@ -8,6 +10,24 @@ public class Main {
 
         int wylosowanaLiczba = (int) (Math.random()*100+1);
         //rzutowanie
+        /*
+        typy proste przechowują tylko wartość
+        logiczne boolean
+        całkowite byte short int long
+        rzeczywiste float double
+        znakowy char
+
+        typy złożóne obiekty własności i metody
+        String Integer Kosc
+         */
         System.out.println(wylosowanaLiczba);
+
+
+        //wczytywanie z klawiatury
+        Scanner klawiatura = new Scanner(System.in);
+        System.out.println("Podaj liczbę");
+        int wpisanaLiczba = klawiatura.nextInt();
+
+
     }
 }
